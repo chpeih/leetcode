a9679bfb8cfbc99055f4bd90e74439259da0d209
gzchenpeihuan <gzchenpeihuan@corp.netease.com>
2014-03-10 12:56:29 +0800

    max points on a line by unordered_map

--- Max Points on a Line.cc
+++ Max Points on a Line.cc
@@ -39,3 +39,34 @@ public:
         return ans;
     }
 };
+//Solution 56ms
+class Solution {
+public:
+    int maxPoints(vector<Point> &points) {
+   	const int n = points.size();
+	if(0==n || 1==n)return n;
+	int ans = -1;
+	int same ,line;
+	unordered_map<float,int> mp;
+	vector<int> visit;
+	for(int i = 0;i<n;i++)visit.push_back(0);
+	for(int i = 0;i<n;i++)if(!visit[i]){
+		same = 1;
+		line = 0;
+		mp.clear();
+		visit[i] = 1;
+		for(int j = 0;j<n;j++)if(i!=j){
+			if(points[i].x==points[j].x && points[i].y==points[j].y)same++,visit[j] = 1;
+			else if(points[i].x==points[j].x)line++;
+			else{
+				float k = ((float)points[i].y-points[j].y)/(points[i].x-points[j].x);
+				mp[k]++;
+			}
+		}
+		int cnt = line;
+		for (unordered_map<float,int>::iterator it = mp.begin();it!=mp.end();it++)cnt = max(cnt,it->second);
+		ans = max(ans,same+cnt);
+	}
+	return ans;
+   } 
+};
