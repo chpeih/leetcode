8ad6dca32746d5107576530ddcc46da7ac184342
chpeih <peihuanchen01@gmail.com>
2018-01-15 16:13:54 +0800

    #111:wq

---
+++ medianOfTwoSort.cc
@@ -0,0 +1,40 @@
+
+
+class Solution {
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		int num1 = nums1.size(), num2 = nums2.size();
+		int num = num1 + num2;
+		if(num == 0) return 0;
+		if(num%2){
+			return find(num1, 0, num1-1, num2, 0, num2-1, (num+1)/2);
+
+		} else {
+			return (find(num1, 0, num1-1, num2, 0, num2-1, (num+1)/2) + find(num1, 0, num1-1, num2, 0, num2-1, (num+1)/2) )/2.0;
+		}
+	}
+
+	int find(vector<int>& nums1, int st1, int ed1, vector<int> &nums2, int st2, int ed2, int k)
+	{
+		if(ed1<st1)return nums2[st2+k-1];
+		if(ed2<st2)return nums1[st1+k-1];
+		if(k==1){
+			int k1 = nums1[st1];
+			int k2 = nums2[st2];
+			return k1<k2?k1:k2;
+		}
+		int m1 = k/2;
+		if(m1>ed1-st1+1){
+			m1 = ed1-st1+1;
+		}
+		int m2 = k - m1;
+
+		int v1 = nums1[st1+m1-1];
+		int v2 = nums2[st2+m2-1];
+		if(v1<v2){
+			return find(nums1, st1+m1, ed1, nums2, st2, ed2, k-m1);
+		} else {
+			return find(nums1, st1, ed1, nums2, st2+m2, ed2, k-m2);
+		}	
+	}
+};
