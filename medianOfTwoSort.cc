4db04be68b22a4f472581c547cebb39544589b40
chpeih <peihuanchen01@gmail.com>
2018-01-15 16:19:46 +0800

    sdas

--- medianOfTwoSort.cc
+++ medianOfTwoSort.cc
@@ -3,19 +3,20 @@
 class Solution {
 public:
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-		int num1 = nums1.size(), num2 = nums2.size();
-		int num = num1 + num2;
+		int num = nums1.size(), num2 = nums2.size();
 		if(num == 0) return 0;
 		if(num%2){
-			return find(num1, 0, num1-1, num2, 0, num2-1, (num+1)/2);
+			return find(nums1, 0, nums2, 0, (num+1)/2);
 
 		} else {
-			return (find(num1, 0, num1-1, num2, 0, num2-1, (num+1)/2) + find(num1, 0, num1-1, num2, 0, num2-1, (num+1)/2) )/2.0;
+			return (find(nums1, 0, nums2, 0, num/2) + find(nums1, 0, nums2, 0, num/2+1) )/2.0;
 		}
 	}
 
-	int find(vector<int>& nums1, int st1, int ed1, vector<int> &nums2, int st2, int ed2, int k)
+	int find(vector<int>& nums1, int st1, vector<int> &nums2, int st2, int k)
 	{
+		int ed1 = nums1.size() - 1;
+		int ed2 = nums2.size() - 1;
 		if(ed1<st1)return nums2[st2+k-1];
 		if(ed2<st2)return nums1[st1+k-1];
 		if(k==1){
@@ -28,13 +29,12 @@ public:
 			m1 = ed1-st1+1;
 		}
 		int m2 = k - m1;
-
 		int v1 = nums1[st1+m1-1];
 		int v2 = nums2[st2+m2-1];
 		if(v1<v2){
-			return find(nums1, st1+m1, ed1, nums2, st2, ed2, k-m1);
+			return find(nums1, st1+m1, nums2, st2, k-m1);
 		} else {
-			return find(nums1, st1, ed1, nums2, st2+m2, ed2, k-m2);
+			return find(nums1, st1, nums2, st2+m2, k-m2);
 		}	
 	}
 };
